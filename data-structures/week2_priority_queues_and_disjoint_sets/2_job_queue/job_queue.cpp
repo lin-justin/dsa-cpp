3ebfdfa32393d4fd0c0214d5a4fb4dad15e2e29c
Justin Lin <justinhlin@yahoo.com>
2021-01-07 11:06:09 -0800

    Completed Priority Queues and Disjoint Sets

--- data-structures/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.cpp
+++ data-structures/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.cpp
@@ -6,55 +6,162 @@ using std::vector;
 using std::cin;
 using std::cout;
 
-class JobQueue {
- private:
-  int num_workers_;
-  vector<int> jobs_;
+class JobQueue
+{
+  private:
+    int num_workers_;
+    vector<int> jobs_;
 
-  vector<int> assigned_workers_;
-  vector<long long> start_times_;
+    vector<int> assigned_workers_;
+    vector<long long> start_times_;
+    vector<std::pair<int, long long> > task;
 
-  void WriteResponse() const {
-    for (int i = 0; i < jobs_.size(); ++i) {
-      cout << assigned_workers_[i] << " " << start_times_[i] << "\n";
-    }
-  }
-
-  void ReadData() {
-    int m;
-    cin >> num_workers_ >> m;
-    jobs_.resize(m);
-    for(int i = 0; i < m; ++i)
-      cin >> jobs_[i];
-  }
-
-  void AssignJobs() {
-    // TODO: replace this code with a faster algorithm.
-    assigned_workers_.resize(jobs_.size());
-    start_times_.resize(jobs_.size());
-    vector<long long> next_free_time(num_workers_, 0);
-    for (int i = 0; i < jobs_.size(); ++i) {
-      int duration = jobs_[i];
-      int next_worker = 0;
-      for (int j = 0; j < num_workers_; ++j) {
-        if (next_free_time[j] < next_free_time[next_worker])
-          next_worker = j;
+    void WriteResponse() const
+    {
+      for (size_t i {0}; i < jobs_.size(); ++i)
+      {
+        cout << assigned_workers_[i] << " " << start_times_[i] << "\n";
       }
-      assigned_workers_[i] = next_worker;
-      start_times_[i] = next_free_time[next_worker];
-      next_free_time[next_worker] += duration;
     }
-  }
 
- public:
-  void Solve() {
-    ReadData();
-    AssignJobs();
-    WriteResponse();
-  }
+    void ReadData()
+    {
+      int m;
+      cin >> num_workers_ >> m;
+      jobs_.resize(m);
+      for(int i {0}; i < m; ++i)
+      {
+        cin >> jobs_[i];
+      }
+    }
+
+    int parent(int i)
+    {
+      return i / 2;
+    }
+
+    int left_child(int i)
+    {
+      return (2 * i) + 1;
+    }
+
+    int right_child(int i)
+    {
+      return left_child(i) + 1;
+    }
+
+    int minIndex(int idx1, int idx2)
+    {
+      if (task[idx1].second == task[idx2].second)
+      {
+        if (task[idx1].first  < task[idx2].first)
+        {
+          return idx1;
+        }
+        else
+        {
+          return idx2;
+        }
+      }
+      else if (task[idx1].second < task[idx2].second)
+      {
+        return idx1;
+      }
+      else
+      {
+        return idx2;
+      }
+    }
+
+    void SiftDown(int i)
+    {
+      int min_index;
+      int left {left_child(i)};
+      int right {right_child(i)};
+
+      int min_left {i}, min_right {i};
+
+      if (left < num_workers_)
+      {
+        min_left = minIndex(left, i);
+      }
+      if (right < num_workers_)
+      {
+        min_right = minIndex(right, i);
+      }
+
+      if (min_left != i && min_right != i)
+      {
+        min_index = minIndex(min_left, min_right);
+        std::swap(task[min_index], task[i]);
+        SiftDown(min_index);
+      }
+      else if (i != min_left)
+      {
+        min_index = min_left;
+        std::swap(task[min_index], task[i]);
+        SiftDown(min_index);
+      }
+      else if (i !=  min_right)
+      {
+        min_index = min_right;
+        std::swap(task[min_index], task[i]);
+        SiftDown(min_index);
+      }
+    }
+
+    void AssignJobs()
+    {
+      // TODO: replace this code with a faster algorithm.
+      // assigned_workers_.resize(jobs_.size());
+      // start_times_.resize(jobs_.size());
+      // vector<long long> next_free_time(num_workers_, 0);
+      // for (int i {0}; i < jobs_.size(); ++i)
+      // {
+      //   int duration {jobs_[i]};
+      //   int next_worker = 0;
+      //   for (int j {0}; j < num_workers_; ++j)
+      //   {
+      //     if (next_free_time[j] < next_free_time[next_worker])
+      //     {
+      //       next_worker = j;
+      //     }
+      //   }
+      //   assigned_workers_[i] = next_worker;
+      //   start_times_[i] = next_free_time[next_worker];
+      //   next_free_time[next_worker] += duration;
+      // }
+
+      assigned_workers_.resize(jobs_.size());
+      start_times_.resize(jobs_.size());
+      task.resize(num_workers_);
+
+      for (int i {0}; i < num_workers_; ++i)
+      {
+        task[i].first = i;
+        task[i].second = 0;
+      }
+
+      for (size_t i {0}; i < jobs_.size(); ++i)
+      {
+        assigned_workers_[i] = task[0].first;
+        start_times_[i] = task[0].second;
+        task[0].second += jobs_[i];
+        SiftDown(0);
+      }
+    }
+
+  public:
+    void Solve()
+    {
+      ReadData();
+      AssignJobs();
+      WriteResponse();
+    }
 };
 
-int main() {
+int main()
+{
   std::ios_base::sync_with_stdio(false);
   JobQueue job_queue;
   job_queue.Solve();
--- data-structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.cpp
+++ data-structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.cpp
@@ -10,56 +10,88 @@ using std::endl;
 using std::max;
 using std::vector;
 
-struct DisjointSetsElement {
+struct DisjointSetsElement
+{
 	int size, parent, rank;
 	
 	DisjointSetsElement(int size = 0, int parent = -1, int rank = 0):
 	    size(size), parent(parent), rank(rank) {}
 };
 
-struct DisjointSets {
+struct DisjointSets
+{
 	int size;
 	int max_table_size;
 	vector <DisjointSetsElement> sets;
 
-	DisjointSets(int size): size(size), max_table_size(0), sets(size) {
-		for (int i = 0; i < size; i++)
+	DisjointSets(int size): size(size), max_table_size(0), sets(size)
+	{
+		for (int i {0}; i < size; i++)
+		{
 			sets[i].parent = i;
+		}
 	}
 
-	int getParent(int table) {
-		// find parent and compress path
+	int getParent(int table)
+	{
+		return sets[table].parent == table ? table : sets[table].parent = getParent(sets[table].parent);
 	}
 
-	void merge(int destination, int source) {
+	void Union(size_t i, size_t j)
+	{
+		if (sets[i].rank < sets[j].rank)
+		{
+			sets[i].parent = j;
+		}
+		else
+		{
+			sets[j].parent = i;
+			if (i == j)
+			{
+				sets[i].rank += 1;
+			}
+		}
+		
+		sets[i].size += sets[j].size;
+		sets[j].size = 0;
+		max_table_size = max(max_table_size, sets[i].size);
+	}
+
+	void merge(int destination, int source)
+	{
 		int realDestination = getParent(destination);
 		int realSource = getParent(source);
-		if (realDestination != realSource) {
+		if (realDestination != realSource)
+		{
 			// merge two components
 			// use union by rank heuristic
-                        // update max_table_size
+            // update max_table_size
+			this->Union(realDestination, realSource);
 		}		
 	}
 };
 
-int main() {
+int main()
+{
 	int n, m;
 	cin >> n >> m;
 
 	DisjointSets tables(n);
-	for (auto &table : tables.sets) {
+	for (auto &table : tables.sets)
+	{
 		cin >> table.size;
 		tables.max_table_size = max(tables.max_table_size, table.size);
 	}
 
-	for (int i = 0; i < m; i++) {
+	for (int i {0}; i < m; i++)
+	{
 		int destination, source;
 		cin >> destination >> source;
                 --destination;
                 --source;
 		
 		tables.merge(destination, source);
-	        cout << tables.max_table_size << endl;
+	    cout << tables.max_table_size << endl;
 	}
 
 	return 0;
